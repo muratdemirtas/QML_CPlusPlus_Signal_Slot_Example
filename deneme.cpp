eacf7f18830648ea364c9abd574e7328d89f52b9
Murat Demirtaş <muratdemirtastr@gmail.com>
2017-04-23 20:39:11 +0300

    Update deneme.cpp

--- deneme.cpp
+++ deneme.cpp
@@ -1,17 +1,20 @@
-#include "deneme.h"
-#include <QDebug>
+#include "deneme.h"  //include header of deneme class
+#include <QDebug>    //include for qdebug function
 
+//create  class constructor
 deneme::deneme(QObject *parent) : QObject(parent)
 {
 
 }
 
 
-
+//this function will receive text from qml form and send response to qml from this class
 void deneme::receive(QString x)
 {
+    //notify us...
     qDebug() << "QT C++:: received variable from QML Form: " << x;
+    //create response packets
     QVariant f = "Thanks From QT C++";
-    emit sendMessageToQML(f);
+    emit sendMessageToQML(f);   //emit signal for qml
 }
 
